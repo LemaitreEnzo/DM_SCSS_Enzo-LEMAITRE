1109c16f856f6ce8340f25b66415f5735bed6c09
Enzo LEMAITRE <lemaitreenzo05@gmail.com>
2025-02-09 14:07:03 +0100

    Add dark mode

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,24 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuIcon = document.querySelector(".header__menuIcon");
     menuIcon.addEventListener("click", toggleMenu);
 });
+
+
+// Dark mode
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggle-dark-mode");
+    const body = document.body;
+
+    // Vérifier le mode enregistré
+    if (localStorage.getItem("dark-mode") === "enabled") {
+        body.classList.add("dark");
+    }
+
+    toggleButton.addEventListener("click", () => {
+        body.classList.toggle("dark");
+        if (body.classList.contains("dark")) {
+            localStorage.setItem("dark-mode", "enabled");
+        } else {
+            localStorage.removeItem("dark-mode");
+        }
+    });
+});
