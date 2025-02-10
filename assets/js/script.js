b11d28cdcb8ae0f5a1199cd52c0999d712cf5605
Lemaitre Enzo <148047686+LemaitreEnzo@users.noreply.github.com>
2025-02-10 17:53:58 +0100

    Update colors

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,11 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
     menuIcon.addEventListener("click", toggleMenu);
 });
 
+const body = document.body;
 
 // Dark mode
 document.addEventListener("DOMContentLoaded", () => {
     const toggleButton = document.getElementById("toggle-dark-mode");
-    const body = document.body;
 
     // Vérifier le mode enregistré
     if (localStorage.getItem("dark-mode") === "enabled") {
@@ -27,4 +27,28 @@ document.addEventListener("DOMContentLoaded", () => {
             localStorage.removeItem("dark-mode");
         }
     });
+});
+
+// Modal
+document.getElementById("modal").addEventListener("click", () => {
+    const modalOverlay = document.querySelector(".modal-overlay");
+    const closeModal = document.querySelector(".modal-close");
+
+    function openModal() {
+        modalOverlay.classList.add("active");
+        body.classList.add("active");
+        body.style.overflow = "hidden";
+    }
+
+    function closeModalHandler() {
+        modalOverlay.classList.remove("active");
+        body.style.overflow = "auto";
+    }
+
+    closeModal.addEventListener("click", closeModalHandler);
+    modalOverlay.addEventListener("click", (e) => {
+        if (e.target === modalOverlay) closeModalHandler();
+    });
+
+    openModal();
 });
